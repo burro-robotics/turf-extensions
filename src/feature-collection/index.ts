a748206e5552ce7ca58af6422dad895f934e962e
Paul Jones <paul@burro.ai>
2025-06-27 16:58:23 -0400

    export first-line-intersect-filtering-endpoints

--- src/feature-collection/index.ts
+++ src/feature-collection/index.ts
@@ -2,5 +2,6 @@ export * from './buffered-convex';
 export * from './clean-feature-collection';
 export * from './find-coordinate';
 export * from './find-line-string';
+export * from './first-line-intersect-filtering-endpoints';
 export * from './line-self-intersects-in';
 export * from './merge-extended-line-strings';
